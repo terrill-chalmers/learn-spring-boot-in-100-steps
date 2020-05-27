efc41033e20c42cd8fe7e71d3fbfb4f50d43f47a
terrill-chalmers <terrill-chalmers@protonmail.com>
2020-05-27 09:43:43 -0500

    first commit

---
+++ src/main/java/io/chalmers/learnspringbootin100steps/LearnSpringBootIn100StepsApplication.java
@@ -0,0 +1,12 @@
+package io.chalmers.learnspringbootin100steps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringBootIn100StepsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnSpringBootIn100StepsApplication.class, args);
+    }
+}
---
+++ src/test/java/io/chalmers/learnspringbootin100steps/LearnSpringBootIn100StepsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.chalmers.learnspringbootin100steps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringBootIn100StepsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
